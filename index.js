d940450d5f68fb3e80db7b7121fd550a8349c83a
Dan MacTough <danmactough@gmail.com>
2020-05-31 23:31:08 -0400

    Add initial implementation

---
+++ index.js
@@ -0,0 +1,27 @@
+const Parser = require('jsonparse');
+const { Transform } = require('stream');
+const { EOL } = require('os');
+
+class ToNDJSON extends Transform {
+  constructor(options = {}) {
+    super(options);
+    const stream = this;
+    const parser = (this._parser = new Parser());
+    parser.onValue = function (value) {
+      if (this.stack.length === 1) {
+        value =
+          options.readableObjectMode === true
+            ? value
+            : JSON.stringify(value) + EOL;
+        stream.push(value);
+      }
+    };
+  }
+
+  _transform(chunk, encoding, callback) {
+    this._parser.write(chunk);
+    callback();
+  }
+}
+
+module.exports = { ToNDJSON };
